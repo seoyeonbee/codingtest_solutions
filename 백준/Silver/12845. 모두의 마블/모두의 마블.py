1b883fe19f5d511d78ac0d622b743a62e138d9dd
seoyeonbee <72788489+seoyeonbee@users.noreply.github.com>
2024-01-03 17:57:22 +0900

    [Silver III] Title: 모두의 마블, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/12845. 모두의 마블/모두의 마블.py
+++ 백준/Silver/12845. 모두의 마블/모두의 마블.py
@@ -1,10 +1,9 @@
 n = int(input()) # 카드 개수
 level = sorted(list(map(int, input().split())), reverse = True) # 카드별 레벨(내림차순 정렬)
 
-max_card = level[0] # 가장 레벨이 큰 카드 저장
-gold = sum(level[:2]) # 골드 저장(가장 레벨이 큰 카드 + 그다음으로 큰 카드 로 초기화)
+gold = 0 # 골드 저장(0으로 초기화)
 
-for i in range(2, n): # 인덱스 2부터 순회
-    gold += (max_card + level[i]) # 가장 레벨이 큰 카드 + 나머지 카드의 레벨을 차례로 누적
+for i in range(1, n): # 레벨이 가장 큰 카드의 다음 인덱스부터 순회
+    gold += (level[0] + level[i]) # 가장 레벨이 큰 카드 + 나머지 카드의 레벨을 차례로 누적
     
 print(gold) # 정답 출력
