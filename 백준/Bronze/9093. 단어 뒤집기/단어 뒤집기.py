376c1c127cd336030f007644e164c7f277feefa0
seoyeonbee <72788489+seoyeonbee@users.noreply.github.com>
2024-01-16 18:04:13 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 96 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
@@ -1,12 +1,11 @@
+import sys
 t = int(input()) # 테스트케이스 수
 
-for _ in range(t): # 각 테스트케이스별 반복 수행
-    s = list(input().split()) # 입력받은 문장을 공백 기준으로 쪼갠 후 리스트화
-    
-    for w in s: # 각 단어별 반복 수행
-        word = list(w) # 현재 단어를 한글자씩 분리해서 리스트화
-        word.reverse() # 모든 글자 뒤집기
-        word_rev = ''.join(word) # 뒤집은 글자들을 다시 합쳐서 한 단어로 저장
-        print(word_rev, end = ' ') # 단어를 공백으로 이어서 출력
-        
-    print() # 줄바꿈
+for _ in range(t): # 각 테스트케이스 별 반복 수행
+		s = list(sys.stdin.readline().rstrip().split()) # 문장 입력받은 후 공백 기준으로 분리해서 리스트화
+		word_rev = [] # 뒤집힌 단어들을 저장할 리스트 생성
+
+		for w in s: # 각 단어 별 반복 수행
+				word_rev.append(w[::-1]) # 역순으로 슬라이싱해서 생성된 새로운 단어를 word_rev에 추가
+
+		print(' '.join(word_rev)) # 단어들을 공백기준으로 합쳐서 출력
